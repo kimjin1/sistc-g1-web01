602777ad94dc0870b8c867216bd93d287ca52dbd
riovalto <riovalto@d17d968b-6666-d738-0a48-ff77c12413be>
2011-10-19 10:53:36 +0000

    - 서블릿 삭제전 백업 - 현재 달력 출력 부분 완성

--- trunk/WebProject01-HAND/WebContent/diary/diary.jsp
+++ trunk/WebProject01-HAND/WebContent/diary/diary.jsp
@@ -5,8 +5,6 @@
 <%
 	int year = myCalendar.getCurrentYear();
 	int month = myCalendar.getCurrentMonth();
-	// 배열의 첫번째값은 마지막일자, 두번째 값은 시작일의 DAY_OF_WEEK
-	int today[] = myCalendar.todayCalendar(year, month);
 %>    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -22,24 +20,11 @@
 				<th colspan=2 height=10%><%=year%>년&nbsp;<%=month%>월&nbsp;달력</th>
 			</tr>
 			<tr>	
-				<td width=80% height=90%>
-					<table width=100% height=100% border=1>
-					<%
-						for(int i=0; i<6; i++){
-						%>
-							<tr>	
-						<%
-							for(int j=0; j<7; j++){
-							%>
-								<td>1</td>
-							<%
-							}
-						%>
-							</tr>
-						<%
-						}
-					%>
-					</table>
+				<td width=80% height=90%>					 
+					<jsp:forward page="/CalendarServlet">
+						<jsp:param value='<%=year%>' name="year"/>
+						<jsp:param value='<%=month%>' name="month"/>
+					</jsp:forward>					 					
 				</td>
 				<td width=20% height=90%>
 					&nbsp;
--- trunk/WebProject01-HAND/src/com/diary/CalendarServlet.java
+++ trunk/WebProject01-HAND/src/com/diary/CalendarServlet.java
@@ -1,6 +1,8 @@
 package com.diary;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,11 +19,59 @@ public class CalendarServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		
+		String strYear = request.getParameter("year");
+		String strMonth = request.getParameter("month");
+		String[] week = {"일", "월", "화", "수", "목", "금", "토"}; 
+		int year = Integer.parseInt(strYear);
+		int month = Integer.parseInt(strMonth);
+		
+		DiaryCalendar myDC = new DiaryCalendar();
+		int result[] = myDC.todayCalendar(year, month);
+		int startDate = 1;
+		int endDate = result[0];
+		int offset = result[1];
+		
+		String output = "<table border=1 width=50% height=50%>";
+		// 요일 표시
+		for(int i=0; i<7; i++){
+			output += "<th bgcolor=#ccccff>";
+			if(i==0){
+				output += "<font color=red>";
+				output += week[i]; 
+				output += "</font>";
+			}else if(i==6){
+				output += "<font color=blue>";
+				output += week[i]; 
+				output += "</font>";				
+			}else{
+				output += week[i];
+			}
+			output += "</th>";			
+		}
+		for(int i=0; i<6; i++){
+			output += "<tr>";
+			for(int j=0; j<7; j++){
+				output += "<td valign=top>";
+				if(i == 0 && j < offset-1){
+					output += "&nbsp;";
+				}else if(startDate <= endDate){
+					output += startDate;
+					startDate++;					
+				}else{
+					output += "&nbsp;";
+				}
+				output += "</td>";
+			}
+			output += "\n"+"</tr>";
+		}
+		output += "</table>";
+		out.println(output);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-
 }
